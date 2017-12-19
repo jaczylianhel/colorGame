feec66530fbfcee75b927129945169606610e039
jaczylianhel <jaczylianhel@gmail.com>
2017-12-19 20:42:39 +0100

    finish the game

--- docs/pick.js
+++ docs/pick.js
@@ -1,47 +1,76 @@
-var colors = randomSquareColors(6);
+var numSquares = 6
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.getElementById("message");
-var pickedColor = randomColor();
 var h1 = document.querySelector("h1");
 var reset = document.querySelector(".reset");
+var mode = document.querySelectorAll(".mode");
 
+init();
 
-reset.addEventListener("click", function(){
-	//generowanie nowych kolorów w kwadratach
-	colors = randomSquareColors(6);
-	//generowanie nowego koloru do trafienia
-	pickedColor = randomColor();
-	//przypisanie tekstu rgb w html 
-	colorDisplay.textContent = pickedColor;
-	//zmiana aktualnych kwadratów na nowe kolory
+function init() {
+	//sprawdzanie easy lub hard mode
+	modeSetupButton();
+	// sprawdzanie kliknietego kwadratu
+	modeClickButton();
+	// restart
+	resetMode();
+}
+
+function modeSetupButton() {
+	for (var i = 0; i < mode.length; i++) {
+		mode[i].addEventListener("click", function() {
+			mode[0].classList.remove("selected");
+			mode[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent === 'Easy' ? numSquares = 3 : numSquares = 6;
+			resetMode()
+		});
+	};
+}
+
+function modeClickButton() {
 	for (var i = 0; i < squares.length; i++) {
-		squares[i].style.backgroundColor = colors[i];
-	}
-	h1.style.backgroundColor = '#232323';
+		squares[i].addEventListener("click", function(){
+			var selected = this.style.backgroundColor;
+			if (selected === pickedColor) {
+				message.textContent = "correct";
+				changeColors(pickedColor);
+				h1.style.backgroundColor = pickedColor;
+				reset.textContent = "New Game";
+			} else {
+				this.style.backgroundColor = '#232323';
+				message.textContent = "try again";
+			}
+		});
+	};
+}
+
+function resetMode() {
+
+	colors = randomSquareColors(numSquares);
+	pickedColor = randomColor();
+	//przypisanie tekstu rgb w html
+	colorDisplay.textContent = pickedColor;
 	message.textContent = "";
 	reset.textContent = "New colors";
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.display = 'block';
+ 			squares[i].style.backgroundColor = colors[i];
+ 		} else {
+ 			squares[i].style.display = 'none';
+ 		}
+	}
+	h1.style.backgroundColor = 'steelblue';
+}
 
+reset.addEventListener("click", function(){
+	resetMode();
 });
 
-//przypisanie tekstu rgb w html
-colorDisplay.textContent = pickedColor;
-
-for (var i = 0; i < squares.length; i++) {
-	squares[i].style.backgroundColor = colors[i];
-	squares[i].addEventListener("click", function(){
-		var selected = this.style.backgroundColor;
-		if (selected === pickedColor) {
-			message.textContent = "correct";
-			changeColors(pickedColor);
-			h1.style.backgroundColor = pickedColor;
-			reset.textContent = "New Game";
-		} else {
-			this.style.backgroundColor = '#232323';
-			message.textContent = "try again";
-		}
-	});
-}
 // funkcja przypisująca trafiony kolor do wszystkich kwadratów
 function changeColors(color) {
 	for(var i = 0; i < colors.length; i++) {
