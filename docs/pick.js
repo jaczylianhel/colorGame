ed6e5357b6a6a338bb7ffbdc6ebe998a8329cd38
jaczylianhel <jaczylianhel@gmail.com>
2017-12-19 18:37:46 +0100

    Create 'new game' button

--- docs/pick.js
+++ docs/pick.js
@@ -1,26 +1,73 @@
-var colors = [
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)"
-]
-
+var colors = randomSquareColors(6);
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.getElementById("colorDisplay");
-var pickedColor = colors[3];
+var message = document.getElementById("message");
+var pickedColor = randomColor();
+var h1 = document.querySelector("h1");
+var reset = document.querySelector(".reset");
+
+
+reset.addEventListener("click", function(){
+	//generowanie nowych kolorów w kwadratach
+	colors = randomSquareColors(6);
+	//generowanie nowego koloru do trafienia
+	pickedColor = randomColor();
+	//przypisanie tekstu rgb w html 
+	colorDisplay.textContent = pickedColor;
+	//zmiana aktualnych kwadratów na nowe kolory
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = '#232323';
+	message.textContent = "";
+	reset.textContent = "New colors";
+
+});
+
+//przypisanie tekstu rgb w html
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
 	squares[i].style.backgroundColor = colors[i];
-
 	squares[i].addEventListener("click", function(){
 		var selected = this.style.backgroundColor;
 		if (selected === pickedColor) {
-			alert ("correct");
+			message.textContent = "correct";
+			changeColors(pickedColor);
+			h1.style.backgroundColor = pickedColor;
+			reset.textContent = "New Game";
 		} else {
-			alert ("wrong");
+			this.style.backgroundColor = '#232323';
+			message.textContent = "try again";
 		}
 	});
+}
+// funkcja przypisująca trafiony kolor do wszystkich kwadratów
+function changeColors(color) {
+	for(var i = 0; i < colors.length; i++) {
+		squares[i].style.backgroundColor = color;
+	};
+}
+
+//funkcja losująca kolor do trafienia
+function randomColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+//funkcja przypisująca kolor rgb do tablicy kolorów
+function randomSquareColors(num) {
+	var arr = [];
+	for (var i = 0; i < num; i++) {
+		arr.push(randomSquareColor());
+	}
+	return arr;
+}
+
+//funkcja tworząca losowy kolor rgb
+function randomSquareColor() {
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
